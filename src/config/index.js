c9fb06283a66acd1252f3ed1331c5dbd7edea3dc
Sruthi-Sunder <Sruthi.Sundararajan@defra.gov.uk>
2024-07-16 16:37:39 +0100

    index change

--- src/config/index.js
+++ src/config/index.js
@@ -76,34 +76,21 @@ const config = convict({
     default: null,
     env: 'CDP_HTTPS_PROXY'
   },
-  // backendApiUrl: {
-  //   doc: 'Backend api url',
-  //   format: String,
-
-  //   default: `https://phi-etl-fera-backend.dev.cdp-int.defra.cloud`,
-  //   env: 'BACKEND_API_URL'
-  // },
-  // frontendUrl: {
-  //   doc: 'Frontend url',
-  //   format: String,
-
-  //   default: `https://phi-frontend.test.cdp-int.defra.cloud`,
-  //   env: 'FRONTEND_URL'
-  // },
+  
   backendApiUrl: {
     doc: 'Backend api url',
     format: String,
-    default: `http://localhost:3049`,
-    // default: `https://phi-etl-fera-backend.dev.cdp-int.defra.cloud`,
+    default: `https://phi-etl-fera-backend.dev.cdp-int.defra.cloud`,
     env: 'BACKEND_API_URL'
   },
   frontendUrl: {
     doc: 'Frontend url',
     format: String,
-    default: `http://localhost:3000`,
-    // default: `https://phi-frontend.test.cdp-int.defra.cloud`,
+    default: `https://phi-frontend.test.cdp-int.defra.cloud`,
     env: 'FRONTEND_URL'
   },
+
+
 
   photoURL: {
     doc: 'photo url',
