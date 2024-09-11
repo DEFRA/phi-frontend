6e68ed8cf36c9eddf7cd1309cda53a341e74d5a5
Sruthi-Sunder <Sruthi.Sundararajan@defra.gov.uk>
2024-09-11 11:43:49 +0100

    observations

--- src/config/index.js
+++ src/config/index.js
@@ -79,13 +79,13 @@ const config = convict({
 
   backendApiUrl: {
     doc: 'Backend api url',
-    format: String,
+    format: String,    
     default: `https://phi-etl-fera-backend.dev.cdp-int.defra.cloud`,
     env: 'BACKEND_API_URL'
   },
   frontendUrl: {
     doc: 'Frontend url',
-    format: String,
+    format: String,   
     default: `https://phi-frontend.test.cdp-int.defra.cloud`,
     env: 'FRONTEND_URL'
   },
