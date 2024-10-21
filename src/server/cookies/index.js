e7beb14ce857230218aba06d11e68f1efb27d0eb
Sruthi-Sunder <Sruthi.Sundararajan@defra.gov.uk>
2024-10-21 13:23:59 +0100

    Login page removal

--- src/server/cookies/index.js
+++ src/server/cookies/index.js
@@ -8,8 +8,8 @@ const CookiesPage = {
         {
           method: 'GET',
           path: '/cookies',
-          ...cookiesController,
-          options: { auth: { mode: 'try' } }
+          ...cookiesController
+          //  options: { auth: { mode: 'try' } }
         }
       ])
     }
