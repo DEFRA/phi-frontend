64b63961f496cfa94cfa613ea2bb7e4d23a3d92e
Abirami Sakthivel <167800061+asakthivel1810@users.noreply.github.com>
2024-11-05 11:33:37 +0000

    Revert "security headers"

--- src/server/common/helpers/errors.js
+++ src/server/common/helpers/errors.js
@@ -21,42 +21,7 @@ function catchAll(request, h) {
   const { response } = request
 
   if (!response.isBoom) {
-    response.header(
-      'Strict-Transport-Security',
-      'max-age=31536000; includeSubDomains'
-    )
-    response.header('X-Content-Type-Options', 'nosniff')
-    response.header('X-Frame-Options', 'DENY')
-    response.header('X-XSS-Protection', '1; mode=block')
-    response.header('Referrer-Policy', 'no-referrer')
-    response.header(
-      'Content-Security-Policy',
-      "default-src 'self' 'sha256-GUQ5ad8JK5KmEWmROf3LZd9ge94daqNvd8xy9YS1iDw=' 'sha256-W+lIm8NzGKSAA+hPYBnTTi9FPX4RS+5f+vn77dO32ko=' 'sha256-pvukEGssf3w6u5+mxgVHnbiZCYAGJG7vMcjE29hkcKs='  https://www.googletagmanager.com; style-src 'self' 'unsafe-inline'"
-    )
-    // COOP (Cross-Origin Opener Policy)
-    response.header('Cross-Origin-Opener-Policy', 'same-origin')
-
-    // COEP (Cross-Origin Embedder Policy)
-    response.header('Cross-Origin-Embedder-Policy', 'require-corp')
-
-    // CORP (Cross-Origin Resource Policy)
-    response.header('Cross-Origin-Resource-Policy', 'same-origin')
-
     return h.continue
-  } else {
-    response.output.headers['Strict-Transport-Security'] =
-      'max-age=63072000; includeSubDomains; preload'
-    response.output.headers['X-Content-Type-Options'] = 'nosniff'
-    response.output.headers['X-Frame-Options'] = 'DENY'
-    response.output.headers['X-XSS-Protection'] = '1; mode=block'
-    response.output.headers['Referrer-Policy'] = 'no-referrer'
-    response.output.headers['Content-Security-Policy'] =
-      "default-src 'self' 'sha256-GUQ5ad8JK5KmEWmROf3LZd9ge94daqNvd8xy9YS1iDw=' 'sha256-W+lIm8NzGKSAA+hPYBnTTi9FPX4RS+5f+vn77dO32ko=' 'sha256-pvukEGssf3w6u5+mxgVHnbiZCYAGJG7vMcjE29hkcKs='  https://www.googletagmanager.com; style-src 'self' 'unsafe-inline'"
-    response.output.header('Cross-Origin-Opener-Policy', 'same-origin')
-    // COEP (Cross-Origin Embedder Policy)
-    response.output.header('Cross-Origin-Embedder-Policy', 'require-corp')
-    // CORP (Cross-Origin Resource Policy)
-    response.output.header('Cross-Origin-Resource-Policy', 'same-origin')
   }
 
   request.logger.error(response)
